7cdb73df49e1b3838e812d8b5aae3fb4531ac9bb
walstromowen <walstrom.owen@gmail.com>
2025-09-12 21:16:08 -0500

    Map Title element fix10

--- frontend/src/controller/overworldController.js
+++ frontend/src/controller/overworldController.js
@@ -102,12 +102,13 @@ export default class OverworldController{
         });
     }
     onSwitchScreen() {
-        if (this.model.props.getSituation() == '') {
+        if (this.model.props.getSituation() === '') {
             this.view.updateMapTitle(this.model.props.getMap().biome.name);
-            this.view.revealMapTitle(); 
+            this.view.revealMapTitle(); // animation plays once
         }
-        this.model.props.setSituation('overworld');
+
         this.view.preloadImages(this.model.props.getMap()).then(() => {
+            this.model.props.setSituation('overworld');
             this.isLooping = true;
             this.model.props.setNextPartyPosition(this.model.props.getCurrentPartyPosition());
             this.view.revealOverworldUi();
